09103dabc832391f8ff5ec1064b87a622b1b7ed7
François Rosato <francois.rosato@ekino.com>
2017-10-03 23:08:14 +0200

    [+] Base code

---
+++ popup/src/scripts/components/app/App.jsx
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  componentDidMount() {
+      document.addEventListener('click', () => {
+        this.props.dispatch({
+            type: 'ADD_COUNT'
+        })
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        test Click Count: {this.props.count}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    count: state.count
+  };
+};
+
+export default connect(mapStateToProps)(App);
