372ee2d01ed87636417723638006d17529fdfd78
François Rosato <francois.rosato@ekino.com>
2017-10-15 21:10:56 +0200

    [*] Folder structure

--- popup/src/index.js
+++ popup/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-import App from './components/app/App'
+import App from './modules/app/App'
 
 import { Store } from 'react-chrome-redux'
 import { Provider } from 'react-redux'
+
 
 const proxyStore = new Store({
     portName: 'example'
